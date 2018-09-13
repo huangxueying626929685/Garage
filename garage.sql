288b7f673b0b83c80efb059179f4b756efe8bec1
LIFAN <874508852@qq.com>
2018-09-13 16:17:27 +0800

    2018-9-13

--- garage.sql
+++ garage.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2018 at 09:17 AM
--- Server version: 5.5.39
+-- Generation Time: 2018-07-05 15:57:38
+-- 服务器版本： 5.5.39
 -- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -23,7 +23,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `danpianji`
+-- 表的结构 `back_danpianji`
+--
+
+CREATE TABLE IF NOT EXISTS `back_danpianji` (
+`id` int(10) NOT NULL,
+  `now_time` varchar(100) COLLATE utf8_bin NOT NULL,
+  `back` varchar(100) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `back_danpianji`
+--
+
+INSERT INTO `back_danpianji` (`id`, `now_time`, `back`) VALUES
+(1, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `danpianji`
 --
 
 CREATE TABLE IF NOT EXISTS `danpianji` (
@@ -35,38 +54,19 @@ CREATE TABLE IF NOT EXISTS `danpianji` (
   `all_floor` varchar(100) COLLATE utf8_bin NOT NULL,
   `datas` varchar(500) COLLATE utf8_bin NOT NULL,
   `len` int(20) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `danpianji`
---
-
-INSERT INTO `danpianji` (`id`, `time1`, `device_id`, `car_num`, `floor_1`, `all_floor`, `datas`, `len`) VALUES
-(1, '2018-06-01 20:09:49', 1, '豫FSQ818', '0', '101010110', '{"id":"1","car":"D4A5FSQ818","f_1":"0","fall":"0","all":"00101010110"}', 70),
-(2, '2018-06-01 20:09:49', 0, '0', '0', '0', '{"id":"1","car":"D4A5FSQ818","f_1":"0","fall":"0","all":"00101010110"}', 70);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `floor_1`
---
-
-CREATE TABLE IF NOT EXISTS `floor_1` (
-`id` int(10) NOT NULL,
-  `high_floor_cp_num` varchar(10) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `floor_1`
+-- 转存表中的数据 `danpianji`
 --
 
-INSERT INTO `floor_1` (`id`, `high_floor_cp_num`) VALUES
-(1, '201');
+INSERT INTO `danpianji` (`id`, `time1`, `device_id`, `car_num`, `floor_1`, `all_floor`, `datas`, `len`) VALUES
+(1, '2018-06-01 20:09:49', 1, '豫FSQ818', '0', '101010110', '{"id":"1","car":"D4A5FSQ818","f_1":"0","fall":"0","all":"00101010110"}', 70);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `garage_info`
+-- 表的结构 `garage_info`
 --
 
 CREATE TABLE IF NOT EXISTS `garage_info` (
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `garage_info` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=5 ;
 
 --
--- Dumping data for table `garage_info`
+-- 转存表中的数据 `garage_info`
 --
 
 INSERT INTO `garage_info` (`id`, `latitude`, `longtitude`, `address`, `total_num`, `free_num`, `price_per_hour`) VALUES
@@ -92,22 +92,22 @@ INSERT INTO `garage_info` (`id`, `latitude`, `longtitude`, `address`, `total_num
 -- --------------------------------------------------------
 
 --
--- Table structure for table `late_order`
+-- 表的结构 `late_order`
 --
 
 CREATE TABLE IF NOT EXISTS `late_order` (
 `id` int(11) NOT NULL,
   `username` varchar(20) COLLATE utf8_bin NOT NULL,
   `car_num` varchar(20) COLLATE utf8_bin NOT NULL,
-  `action_time` varchar(20) COLLATE utf8_bin NOT NULL,
-  `cancel_time` varchar(20) COLLATE utf8_bin NOT NULL,
-  `order_time` varchar(20) COLLATE utf8_bin NOT NULL,
-  `start_time` varchar(20) COLLATE utf8_bin NOT NULL,
+  `action_time` varchar(20) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `cancel_time` varchar(20) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `order_time` varchar(20) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `start_time` varchar(20) COLLATE utf8_bin NOT NULL DEFAULT '0',
   `money` float NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=130 ;
 
 --
--- Dumping data for table `late_order`
+-- 转存表中的数据 `late_order`
 --
 
 INSERT INTO `late_order` (`id`, `username`, `car_num`, `action_time`, `cancel_time`, `order_time`, `start_time`, `money`) VALUES
@@ -116,7 +116,7 @@ INSERT INTO `late_order` (`id`, `username`, `car_num`, `action_time`, `cancel_ti
 -- --------------------------------------------------------
 
 --
--- Table structure for table `manager`
+-- 表的结构 `manager`
 --
 
 CREATE TABLE IF NOT EXISTS `manager` (
@@ -126,7 +126,7 @@ CREATE TABLE IF NOT EXISTS `manager` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `manager`
+-- 转存表中的数据 `manager`
 --
 
 INSERT INTO `manager` (`id`, `username`, `password`) VALUES
@@ -135,7 +135,7 @@ INSERT INTO `manager` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `member`
+-- 表的结构 `member`
 --
 
 CREATE TABLE IF NOT EXISTS `member` (
@@ -145,26 +145,21 @@ CREATE TABLE IF NOT EXISTS `member` (
   `car_num` varchar(20) CHARACTER SET utf8 NOT NULL,
   `car_type` varchar(20) CHARACTER SET utf8 NOT NULL,
   `status` int(10) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=31 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=28 ;
 
 --
--- Dumping data for table `member`
+-- 转存表中的数据 `member`
 --
 
 INSERT INTO `member` (`id`, `username`, `password`, `car_num`, `car_type`, `status`) VALUES
 (1, '18959204245', '123456', '赣F16712', 'Aventador LP700-4', 1),
-(11, '13557399267', '123456', '123', '123', 1),
-(23, '17768105856', '123456', '1234', '4554', 1),
 (26, '18795968928', '123456', '粤CKC236', '白色保时捷', 1),
-(27, '18362903376', '123456', '豫FSQ818', '黑色丰田', 1),
-(28, '', '', '', '', 1),
-(29, '15062223700', '12345678', '苏J123456', 'car', 1),
-(30, '', '', '', '', 1);
+(27, '18362903376', '123456', '豫FSQ818', '黑色丰田', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `no1_car_place`
+-- 表的结构 `no1_car_place`
 --
 
 CREATE TABLE IF NOT EXISTS `no1_car_place` (
@@ -175,7 +170,7 @@ CREATE TABLE IF NOT EXISTS `no1_car_place` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=14 ;
 
 --
--- Dumping data for table `no1_car_place`
+-- 转存表中的数据 `no1_car_place`
 --
 
 INSERT INTO `no1_car_place` (`id`, `cp_num`, `fall_io_point`, `status`) VALUES
@@ -196,7 +191,25 @@ INSERT INTO `no1_car_place` (`id`, `cp_num`, `fall_io_point`, `status`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_history`
+-- 表的结构 `no1_floor_1`
+--
+
+CREATE TABLE IF NOT EXISTS `no1_floor_1` (
+`id` int(10) NOT NULL,
+  `high_floor_cp_num` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `no1_floor_1`
+--
+
+INSERT INTO `no1_floor_1` (`id`, `high_floor_cp_num`) VALUES
+(1, '201');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `order_history`
 --
 
 CREATE TABLE IF NOT EXISTS `order_history` (
@@ -208,30 +221,12 @@ CREATE TABLE IF NOT EXISTS `order_history` (
   `start_time` varchar(20) COLLATE utf8_bin NOT NULL,
   `leave_time` varchar(20) COLLATE utf8_bin NOT NULL,
   `money` float NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=23 ;
-
---
--- Dumping data for table `order_history`
---
-
-INSERT INTO `order_history` (`id`, `username`, `car_num`, `garage_num`, `cp_num`, `start_time`, `leave_time`, `money`) VALUES
-(3, '18959204245', '赣F16712', 4, 101, '2018-05-02 18:30', '2018-05-06 00:36', 0.01),
-(12, '18795968928', '粤CKC236', 1, 101, '2018-05-07 18:11', '2018-05-07 18:12', 0.01),
-(13, '18959204245', '赣F16712', 3, 101, '2018-05-07 19:59', '2018-05-07 19:59', 0.01),
-(14, '18362903376', '苏E9E5F9', 1, 201, '2018-05-09 15:54', '2018-05-09 16:00', 0.01),
-(15, '18362903376', '苏E9E5F9', 1, 201, '2018-05-09 16:17', '2018-05-09 16:29', 0.01),
-(16, '18362903376', '豫FSQ818', 1, 301, '2018-05-11 09:59', '2018-05-11 10:04', 0.01),
-(17, '18795968928', '粤CKC236', 1, 302, '2018-05-11 10:04', '2018-05-11 10:07', 0.01),
-(18, '1414', '141', 0, 1, '', '', 0),
-(19, '11', '1', 1, 1, '1', '1', 1),
-(20, '1', '1', 1, 1, '1', '1', 1),
-(21, '18959204245', '苏A87978', 2, 101, '2018-04-23 10:12', '2018-05-21 22:49', 0.01),
-(22, '18959204245', '豫FSQ818', 1, 101, '2018-05-22 16:44', '2018-05-02 14:35', 0.01);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_info`
+-- 表的结构 `order_info`
 --
 
 CREATE TABLE IF NOT EXISTS `order_info` (
@@ -244,7 +239,7 @@ CREATE TABLE IF NOT EXISTS `order_info` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=229 ;
 
 --
--- Dumping data for table `order_info`
+-- 转存表中的数据 `order_info`
 --
 
 INSERT INTO `order_info` (`order_id`, `username`, `car_num`, `garage_num`, `action_time`, `start_time`) VALUES
@@ -255,7 +250,7 @@ INSERT INTO `order_info` (`order_id`, `username`, `car_num`, `garage_num`, `acti
 -- --------------------------------------------------------
 
 --
--- Table structure for table `parking_info`
+-- 表的结构 `parking_info`
 --
 
 CREATE TABLE IF NOT EXISTS `parking_info` (
@@ -273,19 +268,19 @@ CREATE TABLE IF NOT EXISTS `parking_info` (
   `money` float NOT NULL,
   `pay_status` int(20) NOT NULL,
   `confirm_out` int(10) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=364 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=363 ;
 
 --
--- Dumping data for table `parking_info`
+-- 转存表中的数据 `parking_info`
 --
 
 INSERT INTO `parking_info` (`parking_id`, `username`, `car_num`, `garage_num`, `cp_num`, `finish_parking`, `confirm_parking`, `action_time`, `order_time`, `start_time`, `leave_time`, `money`, `pay_status`, `confirm_out`) VALUES
-(362, '18959204245', '赣F16712', 2, '201', 1, 1, '2018-05-11 09:57', '2018-05-30 10:56', '2018-04-28 18:07', '2018-06-07 10:25', 0.01, 0, 0);
+(362, '18959204245', '赣F16712', 2, '201', 1, 0, '2018-05-11 09:57', '2018-05-30 10:56', '2018-04-28 18:07', '2018-06-07 10:25', 0.01, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `running_data`
+-- 表的结构 `running_data`
 --
 
 CREATE TABLE IF NOT EXISTS `running_data` (
@@ -298,15 +293,15 @@ CREATE TABLE IF NOT EXISTS `running_data` (
 --
 
 --
--- Indexes for table `danpianji`
+-- Indexes for table `back_danpianji`
 --
-ALTER TABLE `danpianji`
+ALTER TABLE `back_danpianji`
  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `floor_1`
+-- Indexes for table `danpianji`
 --
-ALTER TABLE `floor_1`
+ALTER TABLE `danpianji`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -340,6 +335,12 @@ ALTER TABLE `no1_car_place`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `no1_floor_1`
+--
+ALTER TABLE `no1_floor_1`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `order_history`
 --
 ALTER TABLE `order_history`
@@ -362,14 +363,14 @@ ALTER TABLE `parking_info`
 --
 
 --
+-- AUTO_INCREMENT for table `back_danpianji`
+--
+ALTER TABLE `back_danpianji`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `danpianji`
 --
 ALTER TABLE `danpianji`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `floor_1`
---
-ALTER TABLE `floor_1`
 MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `garage_info`
@@ -390,17 +391,22 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
+MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=28;
 --
 -- AUTO_INCREMENT for table `no1_car_place`
 --
 ALTER TABLE `no1_car_place`
 MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
+-- AUTO_INCREMENT for table `no1_floor_1`
+--
+ALTER TABLE `no1_floor_1`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `order_history`
 --
 ALTER TABLE `order_history`
-MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `order_info`
 --
@@ -410,7 +416,7 @@ MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=229;
 -- AUTO_INCREMENT for table `parking_info`
 --
 ALTER TABLE `parking_info`
-MODIFY `parking_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=364;
+MODIFY `parking_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=363;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
